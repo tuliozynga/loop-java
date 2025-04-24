c6505c173e4b8e75603515c740dd5e1703c309ab
Lucas <lucasawagner367@gmail.com>
2025-04-24 18:06:01 -0300

    Add files via upload

---
+++ js/while.js
@@ -0,0 +1,25 @@
+const app = document.getElementById("app")
+const campo = document.getElementById("numero")
+campo.addEventListener("keyup" , (event)=>{
+    console.log(event)
+    console.log(event.target)
+    console.log(event.target.value)
+    do{
+
+    }while(numero <= 0)
+    while(true){
+    if(numero % 2 == 0){
+        break
+    }
+    console.log(numero)
+    if(numero > 678){
+        break
+    }
+}
+})
+const nomes = ["daniel" , "fretz" , "faustino" , "cristiane" , "rodrigo"]
+const idades = [20,33,98,60,33,55,43,22,93,88]
+while(true){
+    numero++
+    console.log(numero)
+}
